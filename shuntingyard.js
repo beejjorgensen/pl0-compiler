b8b2bf4eebfffa2441d81f6c6c30334cb489ac6e
Brian "Beej Jorgensen" Hall <beej@beej.us>
2017-10-26 18:17:32 -0700

    remove commetn:

--- shuntingyard.js
+++ shuntingyard.js
@@ -4,33 +4,6 @@
  * https://en.wikipedia.org/wiki/Shunting-yard_algorithm
  */
 
-/*
-while there are tokens to be read:
-read a token.
-if the token is a number, then push it to the output queue.
-if the token is an operator, then:
-	while there is an operator at the top of the operator stack with
-		greater than or equal to precedence and the operator is left associative:
-			pop operators from the operator stack, onto the output queue.
-	push the read operator onto the operator stack.
-if the token is a left bracket (i.e. "("), then:
-	push it onto the operator stack.
-if the token is a right bracket (i.e. ")"), then:
-	while the operator at the top of the operator stack is not a left bracket:
-		pop operators from the operator stack onto the output queue.
-	pop the left bracket from the stack.
-	** if the stack runs out without finding a left bracket, then there are
-	mismatched parentheses. **
-
-
-if there are no more tokens to read:
-while there are still operator tokens on the stack:
-	** if the operator token on the top of the stack is a bracket, then
-	there are mismatched parentheses. **
-	pop the operator onto the output queue.
-exit.
-*/
-
 const precedence = {
 	'UNARY_MINUS': 0,
 	'UNARY_PLUS': 0,
